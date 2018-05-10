6189cf9ccc3fe4df3136dd4ae96ffbb3b9f419f7
Vincent Karuri <vincentmkaruri@gmail.com>
2018-05-10 15:27:26 +0300

    Rename packages and perform some test refactor

---
+++ app/src/main/java/org/smartregister/mvp/random_quote_generator/presenter/MainPresenter.java
@@ -0,0 +1,11 @@
+package org.smartregister.mvp.random_quote_generator.presenter;
+
+/**
+ * Created by VonBass on 5/1/2018.
+ */
+public interface MainPresenter {
+
+    void onButtonClick();
+
+    void onDestroy();
+}
